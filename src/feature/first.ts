e757ab10c1d15b18281bb5ebc955000e599ec6a6
Dhananjay Manjare <dhananjay.manjare@docquity.com>
2023-10-18 13:06:58 +0530

    first commit

---
+++ src/feature/first.ts
@@ -0,0 +1,4 @@
+const _console = (msg:string,color:string,background:string) =>{
+    console.log("%c".concat(msg), "background: ".concat(background, "; color: ").concat(color));
+}
+export {_console};
---
+++ src/feature/index.ts
@@ -0,0 +1,1 @@
+export {_console} from './first';
