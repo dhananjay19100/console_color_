4c2992fa6e553a461ba2df616cb28a6a36413bff
Dhananjay Manjare <dhananjay.manjare@docquity.com>
2023-10-19 11:24:28 +0530

    s

--- src/feature/first.ts
+++ src/feature/first.ts
@@ -1,5 +1,4 @@
-const _console = (msg:string,color:string,background:string) =>{
+const _console = (msg:string,color?:string,background?:string) =>{
     console.log("%c".concat(msg), "background: ".concat(background, "; color: ").concat(color));
 }
-//export
 export {_console};
